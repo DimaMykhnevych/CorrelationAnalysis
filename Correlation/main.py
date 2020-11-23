378f51fcf4b705938d02cf86f43a26c9ac8aa430
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2020-11-23 15:59:19 +0200

    Finish correlation analysis

--- Correlation/main.py
+++ Correlation/main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+sns.set()
+data = pd.read_csv("Fish.csv")
+data.head()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def show_plot():
+    # logged_hum = np.log(data["Humidity"]);
+    # plt.scatter(logged_hum, data["Temperature"])
+    plt.scatter(data["Weight"], data["Length1"])
+    plt.xlabel("Length", size=14)
+    plt.ylabel("Weight", size=14)
+    plt.show()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def calculate_correlation():
+    data["xy"] = data["Weight"] * data["Length1"]
+    data["x2"] = data["Weight"] ** 2
+    data["y2"] = data["Length1"] ** 2
+    sum_x = sum(data["Weight"])
+    sum_y = sum(data["Length1"])
+    sum_xy = sum(data["xy"])
+    sum_xx = sum(data["x2"])
+    sum_yy = sum(data["y2"])
+    n = data["Length1"].count()
+    r = (n * sum_xy - sum_x * sum_y) / np.sqrt(((n * sum_xx - sum_x ** 2) * (n * sum_yy - sum_y ** 2)))
+    return r
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def calculate_t():
+    r = calculate_correlation()
+    df = data["Length1"].count() * 2 - 2
+    print("Степени свободы: ", df)
+    t = (r * (df ** 0.5))//(1 - r**2)
+    return t
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def main():
+    corel = calculate_correlation()
+    print("Correlation coefficient: ", corel)
+    t = calculate_t()
+    print("t: ", t)
+    print(t, ">1.968. Нулевую теорию отбрасываем")
+    show_plot()
+
+
+if __name__ == "__main__":
+    main()
